18db28555e31ce519ed6d43c7ab6748d7ad3979b
Gazelle608 <pearsongazelle@gmail.com>
2026-07-17 14:17:53 +0200

    Worked in the Features/auth/ folder. Added in a LoginForm.tsx and LoginPage.tsx. I also installed the react-router-dom.

---
+++ frontend/src/features/auth/pages/LoginPage.tsx
@@ -0,0 +1,18 @@
+import LoginForm from "../components/LoginForm";
+
+export default function LoginPage() {
+  return (
+    <div
+      className="
+      flex
+      min-h-screen
+      items-center
+      justify-center
+      bg-slate-800
+      px-4
+      "
+    >
+      <LoginForm />
+    </div>
+  );
+}

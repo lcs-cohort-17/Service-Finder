ce8503d1bae2c6996b6ef07a29397bfe07d1003b
Onke Mbingeleli <onkembingeleli@gmail.com>
2026-07-21 12:05:02 +0200

    chore: Remove _old_jsx_SuggestPlaceForm folder

--- frontend/_DELETE_ME_old_jsx_SuggestPlaceForm/index.js
+++
@@ -1,6 +0,0 @@
-/** @addsuggestions-005-author Onke Mbingeleli*/
-export { SuggestPlaceButton } from './SuggestPlaceButton';
-export { SuggestPlaceModal } from './SuggestPlaceModal';
-export { LoginPromptModal } from './LoginPromptModal';
-export { SuggestToast } from './SuggestToast';
-export { HoursModal, DAYS } from './HoursModal';
